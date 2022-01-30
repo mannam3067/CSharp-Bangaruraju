53f781567628051821ac8558eb9849983b784393
mallikharjuna.mannam <m.malli0005@gmail.com>
2022-01-30 21:50:41 +0530

    Added new class variable declaration and initialized

--- AccessModifiers/VariablesDemo.cs
+++ AccessModifiers/VariablesDemo.cs
@@ -8,9 +8,23 @@ namespace AccessModifiers
 {
     class VariablesDemo
     {
+        int x;   //Non Static
+        static int y=200;  //Static
+        const string pi = "A const field requires a value to be provided AccessModifiers"; //A const field requires a value to be provided AccessModifiers; 
+        readonly int ProjectNumber;
+        public VariablesDemo(int Projetnumber)
+        {
+            ProjectNumber = Projetnumber;
+        }
         static void Main(string[] args)
         {
-
+            y = 100;
+            VariablesDemo vd = new VariablesDemo(660373);
+            vd.x = 200;
+            Console.WriteLine(y);
+            // A readonly field cannot be assigned to(except in a constructor or init - only setter of the class in which the field is defined or a variable initializer))
+            //vd.ProjectNumber = 65749; 
+            Console.ReadLine();
         }
     }
 }
